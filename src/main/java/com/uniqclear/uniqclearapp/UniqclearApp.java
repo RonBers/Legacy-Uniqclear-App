0648e2ee8468ea573619df30c192b19921560689
RonBersabal <93243154+RonBers@users.noreply.github.com>
2023-09-09 11:29:53 +0800

    uniqclear app P1

---
+++ src/main/java/com/uniqclear/uniqclearapp/UniqclearApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uniqclear.uniqclearapp;
+import lognDashboardPackage.loginPage;
+/**
+ *
+ * @author ronjoshual.bersabal
+ */
+public class UniqclearApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        loginPage login =  new loginPage();
+        login.setVisible(true);
+    }
+}
---
+++ src/main/java/connectionSql/mysqlConnection.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connectionSql;
+import java.sql.*;
+/**
+ *
+ * @author ronjoshual.bersabal
+ */
+public class mysqlConnection {
+    Connection con;
+    
+    
+    public mysqlConnection(){
+        
+        String url ="jdbc:mysql://localhost/uniqclearDB";
+        String user="root";
+        String pass="uniqclearDB";
+        
+        try{
+            con = DriverManager.getConnection(url,user,pass);
+        }catch(Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    
+    
+    public Connection getCon(){
+        return con;
+    }
+}
