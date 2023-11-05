94776d6aca969a6f100bbbd3bef50444edfa0633
Fhelry <137862066+FCPongs@users.noreply.github.com>
2023-11-05 11:31:11 +0800

    PromoPackage and ContractPackage updates (#3)
    * promoPackage added, promoTable added
    
    * wrong file added
    
    * promoPackage added, promoTable added
    
    * add promoPackage and promo table
    
    * new changes
    
    * addItem for already added, also connected to Inventory page, need connection for database pa
    
    * Update mysqlConnection.java
    
    String url line changed to solve conflict
    
    * addPromo and addItem already working, no validations of input yet
    
    * addItem working
    
    * newContract edited for inputs, plainly GUI
    
    * [Test] Updated GUI for newContract
    
    * Input validation: Only accepts gForms and gDrive links, editedJOPtion messages(For invalid link and null date
    
    * tablesPromo: Working table and working edit button (Opens other frame)
    editPromoRecord: Displays the selected customer, customer info is editable
    
    ---------
    
    Co-authored-by: RonBersabal <93243154+RonBers@users.noreply.github.com>

--- src/main/java/connectionSql/mysqlConnection.java
+++ src/main/java/connectionSql/mysqlConnection.java
@@ -14,9 +14,11 @@ public class mysqlConnection {
     
     public mysqlConnection(){
         
-        String url ="jdbc:mysql://localhost/uniqcleardb";
+
+        String url ="jdbc:mysql://localhost/uniqcleardbv9";
+
         String user="root";
-        String pass="uniqclearDB";
+        String pass="12345";
         
         try{
             con = DriverManager.getConnection(url,user,pass);
